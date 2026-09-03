c6ed03552331c3e9fccc630ed550eaf4ae61c7e0
Miepee <janbidler00@protonmail.com>
2026-09-03 15:43:01 +0200

    always fail when trying to read/write invalid chars

--- test/stubs/global.h
+++ test/stubs/global.h
@@ -1,19 +1,38 @@
 #include "../../kernel/global.h"
 #include <stdio.h>
+#include "unity.h"
+
+static inline void checkValidAddress(u32 address)
+{
+    if (address < P2C(0x80000000) || address >= P2C(0x82400000))
+    {
+        TEST_FAIL_MESSAGE("Invalid reading/writing address.");
+    }
+}
+
+#define VERBOSE_PRINTS true
 
 #undef read8
 #define read8(addr) read8_stub(addr)
 static inline u8 read8_stub(u32 addr)
 {
-	printf("read8_stub called with addr: 0x%08X\n", addr);
-	return 0;
+    if (VERBOSE_PRINTS)
+    {
+        printf("read8_stub called with addr: 0x%08X\n", addr);
+    }
+    checkValidAddress(addr);
+    return 0;
 }
 
 #undef read32
 #define read32(addr) read32_stub(addr)
 static inline u32 read32_stub(u32 addr)
 {
-    printf("read32_stub called with addr: 0x%08X\n", addr);
+    if (VERBOSE_PRINTS)
+    {
+        printf("read32_stub called with addr: 0x%08X\n", addr);
+    }
+    checkValidAddress(addr);
     return 0;
 }
 
@@ -21,5 +40,9 @@ static inline u32 read32_stub(u32 addr)
 #define write32(addr, data) write32_stub(addr, data)
 static inline void write32_stub(u32 addr, u32 data)
 {
-    printf("write32_stub called with addr: 0x%08X, data: 0x%08X\n", addr, data);
+    if (VERBOSE_PRINTS)
+    {
+        printf("write32_stub called with addr: 0x%08X, data: 0x%08X\n", addr, data);
+    }
+    checkValidAddress(addr);
 }
